af24b5e644d74ea3b11cc9719b5a1f853d68531d
Furkan <foaltycoder@gmail.com>
2023-12-22 13:48:46 +0300

    Initial Commit

---
+++ NoktaMedyaCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoktaMedyaCase
+//
+//  Created by Furkan Tümay on 12/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
